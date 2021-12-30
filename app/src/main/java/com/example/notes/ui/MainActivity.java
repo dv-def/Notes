a107c6dadb2693ea2c730564dcd095d255036728
Dmitriy Negrobov <ndv.def@gmail.com>
2021-12-31 00:25:09 +0300

    add landscape orientation

--- app/src/main/java/com/example/notes/ui/MainActivity.java
+++ app/src/main/java/com/example/notes/ui/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.notes.ui;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.notes.R;
 import com.example.notes.data.note.Note;
@@ -15,8 +20,6 @@ import com.example.notes.ui.note.NoteListFragment;
 
 public class MainActivity extends AppCompatActivity implements NoteListFragment.NoteListController, EditNoteFragment.EditNoteController {
     public static String NOTE_EXTRA = "NOTE_EXTRA";
-
-    private final int CONTAINER_ID = R.id.main_activity_fragment_host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +53,16 @@ public class MainActivity extends AppCompatActivity implements NoteListFragment.
     @Override
     public void onNoteEdited() {
         initListNoteFragment();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            findViewById(R.id.main_activity_edit_note_fragment_host).setVisibility(View.GONE);
+            findViewById(R.id.main_activity_note_list_fragment_host).getLayoutParams().width = ConstraintLayout.LayoutParams.MATCH_PARENT;
+        }
     }
 
     private void initListNoteFragment() {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(CONTAINER_ID, new NoteListFragment())
+                .replace(R.id.main_activity_note_list_fragment_host, new NoteListFragment())
                 .commit();
     }
 
@@ -67,10 +74,33 @@ public class MainActivity extends AppCompatActivity implements NoteListFragment.
             fragment = new EditNoteFragment();
         }
 
+        int containerId;
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            FrameLayout noteEditLayout = findViewById(R.id.main_activity_edit_note_fragment_host);
+            noteEditLayout.setVisibility(View.VISIBLE);
+
+            FrameLayout noteListLayout = findViewById(R.id.main_activity_note_list_fragment_host);
+            noteListLayout.getLayoutParams().width = 0;
+
+            containerId = R.id.main_activity_edit_note_fragment_host;
+        } else {
+            containerId = R.id.main_activity_note_list_fragment_host;
+        }
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(CONTAINER_ID, fragment)
+                .replace(containerId, fragment)
                 .addToBackStack(null)
                 .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        FragmentManager manager = getSupportFragmentManager();
+        if (manager.findFragmentById(R.id.main_activity_edit_note_fragment_host) == null) {
+            FrameLayout noteListLayout = findViewById(R.id.main_activity_note_list_fragment_host);
+            noteListLayout.getLayoutParams().width = ConstraintLayout.LayoutParams.MATCH_PARENT;
+        }
     }
 }
