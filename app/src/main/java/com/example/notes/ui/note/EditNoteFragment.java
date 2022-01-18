3bcabf1ae23e1154555fcf14f4d4aebd0719a61b
Dmitriy Negrobov <ndv.def@gmail.com>
2022-01-18 22:20:47 +0300

    delete unused imports in EditNoteFragment

--- app/src/main/java/com/example/notes/ui/note/EditNoteFragment.java
+++ app/src/main/java/com/example/notes/ui/note/EditNoteFragment.java
@@ -2,22 +2,17 @@ package com.example.notes.ui.note;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.notes.R;
